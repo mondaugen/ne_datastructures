322273f649b6c51d0cbe94563145b4aa937c9c4b
Nichoas Esterer <nicholas.esterer@gmail.com>
2015-08-26 21:37:17 -0400

    added arm cortex m4 makefile.

---
+++ arm_cm4.mk
@@ -0,0 +1,46 @@
+NEDATASTRUCTURES_SRCS_PATH = src
+NEDATASTRUCTURES_SRCS      = $(wildcard $(NEDATASTRUCTURES_SRCS_PATH)/*.c)
+NEDATASTRUCTURES_INC_PATH  = inc
+NEDATASTRUCTURES_OBJS      = $(NEDATASTRUCTURES_SRCS:$(NEDATASTRUCTURES_SRCS_PATH)/%.c=objs/%.o)
+# infortunately, everything rebuilds when only one header is changed
+NEDATASTRUCTURES_DEP       = $(wildcard $(NEDATASTRUCTURES_INC_PATH)/*.h)
+NEDATASTRUCTURES_CFLAGS	= -Ofast
+
+INC		= $(NEDATASTRUCTURES_INC_PATH) 
+CFLAGS	= $(foreach inc,$(INC),-I$(inc))
+
+CFLAGS += -ggdb3 -Wall -ffunction-sections -fdata-sections
+CFLAGS += -mlittle-endian -mthumb -mcpu=cortex-m4
+CFLAGS += -mthumb-interwork -mfloat-abi=hard -mfpu=fpv4-sp-d16
+CFLAGS += -dD
+CFLAGS += -Wno-unused-function
+
+ARFLAGS   = rcs
+
+OBJSDIR = objs
+
+LIBDIR  = lib
+
+CC = arm-none-eabi-gcc
+AR = arm-none-eabi-ar
+
+BIN = $(LIBDIR)/libne_datastructures.a
+
+
+all: $(LIBDIR) $(OBJSDIR) $(BIN) 
+
+$(LIBDIR):
+	if [ ! -d "$(LIBDIR)" ]; then mkdir $(LIBDIR); fi
+
+$(OBJSDIR):
+	if [ ! -d "$(OBJSDIR)" ]; then mkdir $(OBJSDIR); fi
+
+# compile mmdsp 
+$(NEDATASTRUCTURES_OBJS): $(OBJSDIR)/%.o: $(NEDATASTRUCTURES_SRCS_PATH)/%.c $(NEDATASTRUCTURES_DEP)
+	$(CC) -c $(CFLAGS) $< -o $@ $(NEDATASTRUCTURES_CFLAGS)
+
+$(BIN): $(NEDATASTRUCTURES_OBJS)
+	$(AR) $(ARFLAGS) $@ $^
+
+clean:
+	rm -r $(LIBDIR) $(OBJSDIR)
